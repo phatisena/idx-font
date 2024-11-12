bdd54444406147f3ff20924a3901594bc161de73
Phatisena <naphatism@hotmail.com>
2024-11-12 09:59:44 +0700

    12.0.8

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.57/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"12.0.7","target":"arcade","targetVersion":"1.12.57","repo":"phatisena/idx-font"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.12.57/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"12.0.8","target":"arcade","targetVersion":"1.12.57","repo":"phatisena/idx-font"}
 // total=1251543 new=5.96% cached=88.42% other=5.62%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P196063(s) {
+function _main___P471148(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -839,13 +839,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P196063.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P196063.continuations = [  ]
+_main___P471148.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P471148.continuations = [  ]
 
-function _main___P196063_mk(s) {
+function _main___P471148_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P196063, depth: s.depth + 1,
+        parent: s, fn: _main___P471148, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42606,5 +42606,5 @@ const sprites_BaseSpriteSayRenderer__C2260_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, ["transparency16___4356"])
 
-return _main___P196063
+return _main___P471148
 })
